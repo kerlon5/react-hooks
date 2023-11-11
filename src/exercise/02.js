784ed6df0766e39bf11867eb6514f700fef9e69d
Victor Naumov <leadingexpert@gmail.com>
2023-11-11 11:49:02 +0600

    useEffect lazy init

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -5,7 +5,7 @@ import * as React from 'react'
 
 function Greeting({initialName = ''}) {
   // 💰 
-  const [name, setName] = React.useState(window.localStorage.getItem('name') ?? initialName)
+  const [name, setName] = React.useState(() => window.localStorage.getItem('name') ?? initialName)
 
   React.useEffect(()=>{
     window.localStorage.setItem('name', name);
